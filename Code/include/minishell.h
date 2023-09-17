201ec92d0b1700d2d43f7bdbe247b3706526cf7a
Juan Ariza Ordonez <jariza-o@c3r9s2.42malaga.com>
2023-09-17 20:29:28 +0200

    Makefile, minishell.h created. Echo function crated, not finished

---
+++ Code/include/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 14:55:37 by jariza-o          #+#    #+#             */
+/*   Updated: 2023/09/17 19:27:28 by jariza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include "stdio.h" // printf
+
+/* Built-ins */
+/* Echo */
+void	ft_echo(char **argv);
+
+#endif
---
+++ Code/src/built-ins/echo/echo.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/17 15:13:05 by jariza-o          #+#    #+#             */
+/*   Updated: 2023/09/17 20:23:40 by jariza-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../include/minishell.h"
+
+/*
+- tener en cuenta en el split que si es entre comillas el argv puede tener espacios y \t
+- entre argumentos si hay varios espacio no hay que tenerlos en cuenta
+- como se hace para que cuando empiece por " o por ' haga lo del salto y que se pueda escribir
+*/
+
+static int	ft_echo_flag(char **argv);
+
+void	ft_echo(char **argv)
+{
+	int	i;
+
+	i = ft_echo_flag(argv);
+	if (i == 0)
+		i++;
+	while (argv[i])
+	{
+		ft_putstr(argv[i]);
+		i++;
+		if (argv[i])
+			ft_putstr(' ');
+	}
+	if (ft_echo_flag(argv) <= 1)
+		printf("\n");
+}
+
+static int	ft_echo_flag(char **argv)
+{
+	int	i;
+	int	n;
+
+	i = 1;
+	if (argv[1][0] != '-')
+		return (0);
+	while (argv[i])
+	{
+		n = 0;
+		if (argv[i][n] == '-')
+		{
+			n++;
+			while (argv[i][n])
+			{
+				if (argv[i][n] == 'n')
+					i++;
+				else
+					return (i);
+			}
+		}
+		else
+			return (i);
+		i++;
+	}
+	return (i);
+}
