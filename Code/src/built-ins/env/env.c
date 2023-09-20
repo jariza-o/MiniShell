e93f90499deb8412eb49e761a4429c91cc7ac386
Jose Mª Jaen Montesino <105867025+Kendaluski@users.noreply.github.com>
2023-09-20 18:39:33 +0200

    history, pwd and export

--- Code/src/built-ins/env/env.c
+++ Code/src/built-ins/env/env.c
@@ -3,11 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jjaen-mo <jjaen-mo@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 20:26:31 by jariza-o          #+#    #+#             */
-/*   Updated: 2023/09/17 20:26:35 by jariza-o         ###   ########.fr       */
+/*   Updated: 2023/09/20 16:44:31 by jjaen-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
+
+void ft_print_matrix(char **matrix)
+{
+	int cnt;
+
+	cnt = 0;
+	if(!matrix)
+		return;
+	while(matrix[cnt])
+	{
+		printf("%s\n", matrix[cnt]);
+		cnt++;
+	}
+}
