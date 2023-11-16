4fe60041a858eb2de24d36bea52eec00d1f5edcd
Jose Mª Jaen Montesino <105867025+Kendaluski@users.noreply.github.com>
2023-11-16 16:02:02 +0100

    fixed echo error

--- Code/src/built-ins/echo/echo.c
+++ Code/src/built-ins/echo/echo.c
@@ -6,41 +6,11 @@
 /*   By: jjaen-mo <jjaen-mo@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 15:13:05 by jariza-o          #+#    #+#             */
-/*   Updated: 2023/11/15 22:13:02 by jjaen-mo         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:00:59 by jjaen-mo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
-
-/*
-- tener en cuenta en el split que si es entre comillas el argv 
-puede tener espacios y \t
-- entre argumentos si hay varios espacio no hay que tenerlos en cuenta
-- como se hace para que cuando empiece por " o por ' haga lo del salto 
-y que se pueda escribir
-*/
-
-static int	ft_echo_flag(char **argv)
-{
-	int	cnt;
-	int	cnt2;
-
-	cnt = 0;
-	while (argv[cnt])
-	{
-		cnt2 = 0;
-		if (argv[cnt][cnt2] == '-')
-		{
-			cnt2++;
-			while (argv[cnt][cnt2] == 'n')
-				cnt2++;
-			if (argv[cnt][cnt2] == '\0')
-				return (1);
-		}
-		cnt++;
-	}
-	return (0);
-}
 
 static int	ft_status(char *str)
 {
@@ -56,27 +26,59 @@ static int	ft_status(char *str)
 	return (0);
 }
 
-int	ft_is_flag(char **argv, int cnt)
+static int	ft_is_full_n(char *str)
+{
+	int	cnt;
+
+	cnt = 0;
+	if (str[0] == '-')
+	{
+		cnt++;
+		while (str[cnt])
+		{
+			if (str[cnt] != 'n')
+				return (0);
+			cnt++;
+		}
+		if (str[cnt] == '\0')
+			return (1);
+	}
+	return (0);
+}
+
+static int	ft_is_flag(char **argv, int cnt)
 {
 	int	cnt2;
 
 	cnt2 = 1;
-	if (ft_strncmp("-n", argv[cnt], 2) != 0)
+	if (!ft_is_full_n(argv[cnt]))
 		return (0);
 	while (argv[cnt2] && cnt2 != cnt)
 	{
-		if (ft_strncmp("-n", argv[cnt2], 2) != 0)
+		if (!ft_is_full_n(argv[cnt2]))
 			return (0);
 		cnt2++;
 	}
 	return (1);
 }
 
+void	ft_print(char **argv, int cnt)
+{
+	if (ft_status(argv[cnt]) == 1)
+		ft_printf("%i", g_data.exit_status);
+	else
+		ft_putstr(argv[cnt]);
+	if (argv[cnt + 1])
+		write(1, " ", 1);
+}
+
 void	ft_echo(char **argv)
 {
 	int	cnt;
+	int	flag;
 
 	cnt = 1;
+	flag = 0;
 	if (!argv[1])
 	{
 		write(1, "\n", 1);
@@ -84,15 +86,16 @@ void	ft_echo(char **argv)
 	}
 	while (argv[cnt])
 	{
-		if (ft_status(argv[cnt]) == 1)
-			ft_printf("%i", g_data.exit_status);
-		else if (!ft_is_flag(argv, cnt))
-			ft_putstr(argv[cnt]);
+		if (ft_is_flag(argv, cnt))
+		{
+			flag = 1;
+			cnt++;
+			continue ;
+		}
+		ft_print(argv, cnt);
 		cnt++;
-		if (argv[cnt] && !ft_is_flag(argv, cnt) && argv[cnt + 1])
-			write(1, " ", 1);
 	}
-	if (!ft_echo_flag(argv))
+	if (!flag)
 		printf("\n");
 	g_data.exit_status = 0;
 }
